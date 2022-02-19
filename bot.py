9fa8a67bcb6acd8ac5802d084bc9a603f371d3c6
Thanush A A <aathanush@gmail.com>
2022-02-19 17:41:23 +0530

    added gitignore

---
+++ bot.py
@@ -0,0 +1,20 @@
+import discord
+client=discord.Client()
+
+@client.event
+#Command executes when the bot starts functioning
+async def on_ready():
+    print(f"Logged in as {client.user}")
+
+#Executes once a message is recieved
+@client.event
+async def on_message(message):
+    #The bot should not respond to its own messages
+    if message.author==client.user:
+        return
+
+    #When the bot sees the message hello
+    if message.content.lower().startswith("hello"):
+        await message.channel.send("Hello! Nice to meet you :)")
+
+
