64a1dd91e33a89d2273d7c38b73d32549182f973
Thanush A A <aathanush@gmail.com>
2022-02-27 20:14:45 +0530

    Updated nlp.py

--- nlp.py
+++ nlp.py
@@ -1,2 +1,16 @@
+#Importing all modules
 import nltk
 nltk.download('punkt')
+from nltk.stem.porter import PorterStemmer
+
+stemmer=PorterStemmer()
+
+def tokenize(sentence):
+    return nltk.word_tokenize(sentence)
+
+def stem(word):
+    return stemmer.stem(word.lower())
+
+
+
+
