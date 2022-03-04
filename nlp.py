6f9c1500e7693376dac85a535e5caa98e25c1d75
Thanush A A <aathanush@gmail.com>
2022-03-04 18:40:49 +0530

    Update nlp.py

--- nlp.py
+++ nlp.py
@@ -9,27 +9,15 @@ from nltk.stem.porter import PorterStemmer
 stemmer=PorterStemmer()
 
 def tokenize(sentence):
-    """
-
-    :param sentence:
-    :return: tokenizes a sentence
-    """
+    
     return nltk.word_tokenize(sentence)
 
 def stem(word):
-    """
-        :param word:
-        :return: performs stemming of the given word
-    """
+
     return stemmer.stem(word.lower())
 
 def bag_of_words(tokenized_sentence, all_words):
-    """
 
-    :param tokenized_sentence: list of tokenized sentence
-    :param all_words: list of all words
-    :return:
-    """
     tokenized_sentence=[stem(w) for w in tokenized_sentence]
     bag=np.zeros(len(all_words),dtype=np.float32)
     for idx,w in enumerate(all_words):
