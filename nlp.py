7eecf96dcdd73f3bb7703871c66a018c9434877d
Thanush A A <aathanush@gmail.com>
2022-03-04 18:34:20 +0530

    defined bag_of words in nlp

--- nlp.py
+++ nlp.py
@@ -1,5 +1,8 @@
 #Importing all modules
 import nltk
+import numpy as np
+
+
 nltk.download('punkt')
 from nltk.stem.porter import PorterStemmer
 
@@ -12,8 +15,10 @@ def stem(word):
     return stemmer.stem(word.lower())
 
 def bag_of_words(tokenized_sentence, all_words):
-    pass
-
+    tokenized_sentence=[stem(w) for w in tokenized_sentence]
+    bag=np.zeros(len(all_words),dtype=np.float32)
+    for idx,w in enumerate(tokenized_sentence):
+        if
 
 
 
